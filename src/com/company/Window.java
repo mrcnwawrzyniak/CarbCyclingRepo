283d7b5fc82d4005ed275c8764b664f8a5177989
Marcin Wawrzyniak <mrcnwawrzyniak@gmail.com>
2021-03-01 08:55:05 +0100

    MenuBar created

--- src/com/company/Window.java
+++ src/com/company/Window.java
@@ -4,13 +4,18 @@ import javax.swing.*;
 
 public class Window extends JFrame
 {
+	private JMenuBar mMenuBar;
+	private JPanel panel1;
 
 	public Window()
 	{
-		this.setBounds(400, 400, 300, 400);
+		this.setTitle("CarbCycling");
+		this.setBounds(400, 400, 600, 400);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-
+		panel1 = new JPanel();
+		this.getContentPane().add(panel1);
+		panel1.add(mMenuBar);
 
 
 	}
