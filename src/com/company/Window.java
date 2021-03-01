aa68053e75e7da2c68f624c2945b9bfb7e3cac63
Marcin Wawrzyniak <mrcnwawrzyniak@gmail.com>
2021-03-01 08:41:55 +0100

    Created empty Frame

---
+++ src/com/company/Window.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import javax.swing.*;
+
+public class Window extends JFrame
+{
+
+	public Window()
+	{
+		this.setBounds(400, 400, 300, 400);
+		this.setVisible(true);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+
+
+
+	}
+
+}
