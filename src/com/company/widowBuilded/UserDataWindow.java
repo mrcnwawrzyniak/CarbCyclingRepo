07a7dc865c1430a0bb50aa8e3159cc7068dd4079
Marcin Wawrzyniak <mrcnwawrzyniak@gmail.com>
2021-03-04 13:58:19 +0100

    Need to finish implementation of radio button gender

--- src/com/company/widowBuilded/UserDataWindow.java
+++ src/com/company/widowBuilded/UserDataWindow.java
@@ -3,6 +3,9 @@ package com.company.widowBuilded;
 import com.company.implementation.User;
 
 import javax.swing.*;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
+import java.awt.*;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
 
@@ -10,7 +13,12 @@ public class UserDataWindow extends JFrame
 {
 	private JPanel rootPaneUserDataInfo;
 	private JButton applyButton;
-	private JTextField textFieldGender, textFieldAge, textFieldheigth, textFieldWeigth, textFieldPeriod;
+	private JTextField textFieldAge, textFieldheigth, textFieldWeigth, textFieldPeriod;
+
+	private JRadioButton rbMale = new JRadioButton();
+	private JRadioButton rbFemale = new JRadioButton();
+	private final ButtonGroup buttonGroup = new ButtonGroup();
+
 	User user;
 	private String gender;
 	private int age, heigth, weigth, dietPeriod;
@@ -32,15 +40,17 @@ public class UserDataWindow extends JFrame
 	public UserDataWindow(Window window)
 	{
 		initWindow();
+		mainWindow = window;
+
 		textFieldAge.addKeyListener(keyAdapter);
 		textFieldheigth.addKeyListener(keyAdapter);
 		textFieldWeigth.addKeyListener(keyAdapter);
 		textFieldPeriod.addKeyListener(keyAdapter);
-		mainWindow = window;
+
+
 
 		applyButton.addActionListener(e ->
 		{
-			gender = textFieldGender.getText();
 			try
 			{
 				age = Integer.parseInt(textFieldAge.getText());
@@ -63,6 +73,7 @@ public class UserDataWindow extends JFrame
 		this.setBounds(900, 100, 300, 230);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
 	}
 
 	private void createNewUser()
