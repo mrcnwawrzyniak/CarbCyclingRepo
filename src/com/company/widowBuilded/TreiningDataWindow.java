0f6824e9a96ed7265e4ef6eef8d1385aba4d9175
Marcin Wawrzyniak <mrcnwawrzyniak@gmail.com>
2021-03-06 16:50:41 +0100

    Changed buttons into radiobuttons, need to diesable extension of frame

--- src/com/company/widowBuilded/TreiningDataWindow.java
+++ src/com/company/widowBuilded/TreiningDataWindow.java
@@ -7,7 +7,9 @@ import java.awt.event.ActionListener;
 public class TreiningDataWindow extends JFrame
 {
 	private JPanel rootPaneTreiningData;
-	private JButton btnLose, btnKeep, btnIncrease, btnActiv1, btnActiv2, btnActiv3, btnActiv4, btnActiv5, btnActiv6;
+	private JRadioButton radioButtonIcrease, radioButtonLose, radioButtonKeep, radioButton2, radioButton1, radioButton3, radioButton4, radioButton5, radioButton6;
+	private JPanel panel1;
+	private ButtonGroup bg1 = new ButtonGroup(), bg2 = new ButtonGroup();
 
 	public TreiningDataWindow()
 	{
@@ -19,23 +21,35 @@ public class TreiningDataWindow extends JFrame
 	{
 		setContentPane(rootPaneTreiningData);
 		this.setTitle("User Treining Info");
-		this.setBounds(900, 100, 600, 230);
+		this.setExtendedState();
+		this.setBounds(900, 100, 400, 230);
 		this.setVisible(true);
 		this.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+
+		bg1.add(radioButtonIcrease);
+		bg1.add(radioButtonLose);
+		bg1.add(radioButtonKeep);
+
+		bg2.add(radioButton1);
+		bg2.add(radioButton2);
+		bg2.add(radioButton3);
+		bg2.add(radioButton4);
+		bg2.add(radioButton5);
+		bg2.add(radioButton6);
 	}
 
 	private void setAction()
 	{
 		ButtonListener btnListener = new ButtonListener();
-		btnLose.addActionListener(btnListener);
-		btnKeep.addActionListener(btnListener);
-		btnIncrease.addActionListener(btnListener);
-		btnActiv1.addActionListener(btnListener);
-		btnActiv2.addActionListener(btnListener);
-		btnActiv3.addActionListener(btnListener);
-		btnActiv4.addActionListener(btnListener);
-		btnActiv5.addActionListener(btnListener);
-		btnActiv6.addActionListener(btnListener);
+		radioButtonIcrease.addActionListener(btnListener);
+		radioButtonLose.addActionListener(btnListener);
+		radioButtonKeep.addActionListener(btnListener);
+		radioButton2.addActionListener(btnListener);
+		radioButton1.addActionListener(btnListener);
+		radioButton3.addActionListener(btnListener);
+		radioButton4.addActionListener(btnListener);
+		radioButton5.addActionListener(btnListener);
+		radioButton6.addActionListener(btnListener);
 	}
 
 	class ButtonListener extends JButton implements ActionListener
@@ -43,44 +57,44 @@ public class TreiningDataWindow extends JFrame
 		@Override
 		public void actionPerformed(ActionEvent e)
 		{
-			if(e.getSource() == btnIncrease)
+			if(e.getSource() == radioButtonIcrease)
+			{
+
+			}
+			if(e.getSource() == radioButtonLose)
+			{
+
+			}
+			if(e.getSource() == radioButtonKeep)
+			{
+
+			}
+			if(e.getSource() == radioButton2)
+			{
+
+			}
+			if(e.getSource() == radioButton1)
+			{
+
+			}
+			if(e.getSource() == radioButton3)
+			{
+
+			}
+			if(e.getSource() == radioButton4)
+			{
+
+			}
+			if(e.getSource() == radioButton5)
+			{
+
+			}
+			if(e.getSource() == radioButton6)
 			{
 
 			}
 
-			else if(e.getSource() == btnKeep)
-			{
 
-			}
-
-			else if(e.getSource() == btnLose)
-			{
-
-			}
-			else if(e.getSource() == btnActiv1)
-			{
-
-			}
-			else if(e.getSource() == btnActiv2)
-			{
-
-			}
-			else if(e.getSource() == btnActiv3)
-			{
-
-			}
-			else if(e.getSource() == btnActiv4)
-			{
-
-			}
-			else if(e.getSource() == btnActiv5)
-			{
-
-			}
-			else if(e.getSource() == btnActiv6)
-			{
-
-			}
 		}
 	}
 }
